30f58475f1ea4cb2746aaec3a0c4717a820ef80d
ea0273 <76868962+ea0273@users.noreply.github.com>
2021-01-02 21:28:38 +0900

    読み上げ

--- discordbot.py
+++ discordbot.py
@@ -1,26 +1,50 @@
+import discord
 from discord.ext import commands
+import asyncio
 import os
-import traceback
+import subprocess
+import ffmpeg
+from voice_generator import creat_WAV
 
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+client = commands.Bot(command_prefix='.')
+voice_client = None
 
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
-    
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-   
 
-bot.run(token)
+@client.command()
+async def join(ctx):
+    print('#voicechannelを取得')
+    vc = ctx.author.voice.channel
+    print('#voicechannelに接続')
+    await vc.connect()
+
+@client.command()
+async def bye(ctx):
+    print('#切断')
+    await ctx.voice_client.disconnect()
+
+@client.event
+async def on_message(message):
+    msgclient = message.guild.voice_client
+    if message.content.startswith('.'):
+        pass
+
+    else:
+        if message.guild.voice_client:
+            print(message.content)
+            creat_WAV(message.content)
+            source = discord.FFmpegPCMAudio("output.wav")
+            message.guild.voice_client.play(source)
+        else:
+            pass
+    await client.process_commands(message)
+
+
+client.run("bot作成時の時にコピーしたトークン")
