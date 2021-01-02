787c4602a929de4d2b30388a780610315ebc1684
ea0273 <76868962+ea0273@users.noreply.github.com>
2021-01-02 20:50:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,10 +17,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('にゃーん')
-    
     
 bot.run(token)
